b98af956ee55d8c22ef58c6739d1ecb6531dfeed
Marcus Vinicius <marcusvyinicius@gmail.com>
2023-05-09 10:42:15 -0300

    sitezin beta 1

--- src/pages/projetos/Apps/kanban/jsx/index.jsx
+++ src/pages/projetos/Apps/kanban/jsx/index.jsx
@@ -1,21 +1,43 @@
 import "../css/index.css";
 import BackIcon from "../../../components/BackIcon";
+import { useEffect, useState } from "react";
+import { Outlet } from "react-router-dom";
+import KbItemContainer from "./conponents/KbItemContainer";
 
 function Kanban() {
   // =============pro mobile=============
-  
+  useEffect(() => {
+    kbAreas.map(
+      (i) => i.isIn === true && document.getElementById(i.id).click()
+    );
+  }, []);
+
   // mostra onde voce ta
-  const kbAreas = ["kbToDo", "kbDoing", "kbDone"];
+  const kbAreas = [
+    { id: "kbToDo", isIn: true },
+    { id: "kbDoing", isIn: false },
+    { id: "kbDone", isIn: false },
+  ];
+
   const setKbArea = (e) => {
-    kbAreas
-      .filter((i) => i != e.target.id)
-      .map((i) => (document.getElementById(i).style.borderBottom = ""));
-    e.target.style.borderBottom = "solid #970 5px";
+    e.target.className != "kbNav" &&
+      ((e.target.style.borderBottom = "solid #970 5px"),
+      kbAreas
+        .filter((i) => i.id != e.target.id)
+        .map((i) => {
+          document.getElementById(i.id).style.borderBottom = "";
+        }),
+      kbAreas.map((i) =>
+        i.id == e.target.id ? (i.isIn = true) : (i.isIn = false)
+      ));
   };
+
+  const selectedArea = kbAreas.filter((i) => i.isIn === true);
 
   return (
     <div className="Kanban">
       <BackIcon />
+      {/* header */}
       <div className="kbNav" onClick={setKbArea}>
         <span className="kbNavBtn" id="kbToDo">
           À Fazer
@@ -28,7 +50,13 @@ function Kanban() {
         </span>
       </div>
 
-      <div className="KbContainer"></div>
+      <div className="KbContainer">
+        <Outlet />
+      </div>
+
+      <div className="addKbItemBtn">
+        <KbItemContainer id={selectedArea[0].id} />
+      </div>
     </div>
   );
 }
