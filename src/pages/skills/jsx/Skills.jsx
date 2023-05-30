5e4e135003a2bbff6b21d6a9e5c1c6461f9f0c0c
unknown <marcusvyinicius@gmail.com>
2023-05-30 15:31:44 -0300

    ficando bom

--- src/pages/skills/jsx/Skills.jsx
+++ src/pages/skills/jsx/Skills.jsx
@@ -2,6 +2,7 @@ import "../css/Skills.css";
 import html from "/src/imgs/skills/html.png";
 import css from "/src/imgs/skills/css.png";
 import js from "/src/imgs/skills/js.png";
+import python from "/src/imgs/skills/python.png";
 
 function Skills() {
   return (
@@ -14,9 +15,10 @@ function Skills() {
         <p className="skillsSubTitle">Tecnoligias</p>
 
         <div className="skillsImgContainer">
-          <img src={html} alt="" className="skillsImg" />
-          <img src={css} alt="" className="skillsImg" />
-          <img src={js} alt="" className="skillsImg" />
+          <img src={html} alt="html" className="skillsImg" />
+          <img src={css} alt="css" className="skillsImg" />
+          <img src={js} alt="js" className="skillsImg" />
+          <img src={python} alt="python" className="skillsImg" />
         </div>
       </div>
     </div>
